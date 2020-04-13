d4c3d340ea27534728eff18d536fed9558c76ef1
jeff <jeff@busiui.com>
2020-04-13 12:04:13 +0800

    eureka7001\7002\7003

---
+++ cloud-eureka-service7003/src/main/java/com/atguigu/springcloud/EurekaMain7003.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author sunbaojin
+ * @date 2020/4/13 12:00
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7003 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7003.class,args);
+    }
+}
